dfbb606fb1f2b03b4e89fc6f7e4d50b01c58ca69
Umarali <umaralihudoykulov@gmail.com>
2026-04-14 21:48:22 +0500

    Deploymentga tayyor: Dinamik URL va Postgres sozlamalari

--- asosiy/wsgi.py
+++ asosiy/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asosiy.settings')
 
 application = get_wsgi_application()
+app = application  # CHANGED: Vercel aynan 'app' o'zgaruvchisini qidiradi
